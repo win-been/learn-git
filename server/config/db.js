a7ff64fbddcfe00e7d035c1110142219f86448db
winter <lqr2187406566@163.com>
2026-05-30 21:43:31 +0800

    feat: 改数据库名为 deliverydb

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,6 @@ module.exports = {
     HOST: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     PASSWORD: process.env.DB_PASSWORD || "123456",
-    DB: process.env.DB_NAME || "deliveryDB",
+    DB: process.env.DB_NAME || "deliverydb",
     dialect: "mysql",
 }
